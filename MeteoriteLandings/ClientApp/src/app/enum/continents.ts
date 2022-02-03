97c40b9e46719dbaac842aed692029c70b9cd863
Alexandre COSTA MOREIRA <alexcostamoreira.pro@gmail.com>
2022-02-03 23:02:37 +0100

    Delete Angular project

--- MeteoriteLandings/ClientApp/src/app/enum/continents.ts
+++
@@ -1,8 +0,0 @@
-export enum Continents {
-  Africa = "Africa",
-  America = "America",
-  Antartic = "Antartic",
-  Asia = "Asia",
-  Europe = "Europe",
-  Oceania = "Oceania",
-}
--- MeteoriteLandings/ClientApp/src/app/model/data.model.ts
+++
@@ -1,7 +0,0 @@
-export class Data {
-  name!: string;
-  mass!: number;
-  year!: string;
-  reclat!: number;
-  relong!: number;
-}
