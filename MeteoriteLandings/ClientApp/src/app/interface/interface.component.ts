3d5d105d6947345749af3fe987a4e26c16655f45
Fyleek <odecloquement@gmail.com>
2022-02-05 19:23:42 +0100

    Add min/max Mass and Date, Add snackbar, Indentation update

--- MeteoriteLandings/ClientApp/src/app/interface/interface.component.ts
+++ MeteoriteLandings/ClientApp/src/app/interface/interface.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { MatSliderChange } from '@angular/material/slider';
 import { ThemePalette } from '@angular/material/core';
 import { Params } from '../model/params.model';
+import { MatSnackBar, MatSnackBarConfig } from '@angular/material/snack-bar';
 
 export interface Task {
   name: string;
@@ -14,42 +15,62 @@ export interface Task {
 @Component({
   selector: 'app-interface',
   templateUrl: './interface.component.html',
-  styleUrls: ['./interface.component.scss']
+  styleUrls: ['./interface.component.scss'],
 })
 export class InterfaceComponent implements OnInit {
-  
   @Output() sendData = new EventEmitter();
-  
+
+  @Output() cancelEvent = new EventEmitter();
+
   task: Task = {
     name: 'Continents',
     completed: false,
-    color: 'primary',
+    color: 'warn',
     subtasks: [],
   };
 
   fileName?: string;
   file?: File;
 
-  sliderValue = 0;
+  minMass = 0;
+  maxMass = 100000000;
+
+  minDate = 1400;
+  maxDate = 2013;
+
+  actionSnackBar = 'Close';
+  optionsSnackBar!: MatSnackBarConfig;
+
+  launch = false;
+  cancelAnimation = false;
+
+  constructor(private _snackBar: MatSnackBar) {
+    this.optionsSnackBar = new MatSnackBarConfig();
+    this.optionsSnackBar.verticalPosition = 'top';
+    this.optionsSnackBar.horizontalPosition = 'center';
+    this.optionsSnackBar.duration = 4000;
+  }
 
   ngOnInit(): void {
-      const continents = Object.values(Continents)
-        .map((value) => ({ name: value, completed: false, color: "warn" } as Task));
-      this.task.subtasks = continents;
+    const continents = Object.values(Continents).map(
+      (value) => ({ name: value, completed: false, color: 'primary' } as Task)
+    );
+    this.task.subtasks = continents;
   }
 
   setFileInput(event: Event) {
     const input = event.target as HTMLInputElement;
     const files = input.files;
-    if(files){
+    if (files) {
       this.fileName = files[0].name;
       this.file = files[0];
+      this.launch = true;
     }
   }
 
   formatLabel(value: number) {
     if (value >= 1000) {
-      if(value >= 1000000){
+      if (value >= 1000000) {
         return Math.round(value / 1000000) + 'M';
       } else {
         return Math.round(value / 1000) + 'K';
@@ -58,20 +79,37 @@ export class InterfaceComponent implements OnInit {
     return value;
   }
 
-  setSliderValue(event: MatSliderChange) {
-    this.sliderValue = event.value || 0;
+  setMinimumMass(event: MatSliderChange) {
+    this.minMass = event.value || 0;
+  }
+
+  setMaximumMass(event: MatSliderChange) {
+    this.maxMass = event.value || 100000000;
+  }
+
+  setMaximumDate(value: number) {
+    this.maxDate = value;
+  }
+
+  setMinimumDate(value: number) {
+    this.minDate = value;
   }
 
   allComplete: boolean = false;
   updateAllComplete() {
-    this.allComplete = this.task.subtasks != null && this.task.subtasks.every(t => t.completed);
+    this.allComplete =
+      this.task.subtasks != null &&
+      this.task.subtasks.every((t) => t.completed);
   }
 
   someComplete(): boolean {
     if (this.task.subtasks == null) {
       return false;
     }
-    return this.task.subtasks.filter(t => t.completed).length > 0 && !this.allComplete;
+    return (
+      this.task.subtasks.filter((t) => t.completed).length > 0 &&
+      !this.allComplete
+    );
   }
 
   setAll(completed: boolean) {
@@ -79,17 +117,17 @@ export class InterfaceComponent implements OnInit {
     if (this.task.subtasks == null) {
       return;
     }
-    this.task.subtasks.forEach(t => (t.completed = completed));
+    this.task.subtasks.forEach((t) => (t.completed = completed));
   }
 
   getEnableCheckbox() {
-    if(!this.task.subtasks){
+    if (!this.task.subtasks) {
       return [];
     }
-    
+
     let subtasks: string[] = [];
-    this.task.subtasks.forEach(subtask => {
-      if(subtask.completed){
+    this.task.subtasks.forEach((subtask) => {
+      if (subtask.completed) {
         subtasks.push(subtask.name);
       }
     });
@@ -97,15 +135,47 @@ export class InterfaceComponent implements OnInit {
   }
 
   emitData() {
-    if(!this.file){
+    if (!this.file) {
+      this._snackBar.open(
+        'Please select a file',
+        this.actionSnackBar,
+        this.optionsSnackBar
+      );
       return;
     }
     const continents = this.getEnableCheckbox();
     const params: Params = {
       continents: continents,
-      mass: this.sliderValue,
-      file: this.file
+      minMass: this.minMass,
+      maxMass: this.maxMass,
+      minDate: this.minDate,
+      maxDate: this.maxDate,
+      file: this.file,
+    };
+    if (this.minMass < this.maxMass) {
+      if (this.minDate < this.maxDate) {
+        this.sendData.emit(params);
+        this.cancelAnimation = true;
+        this.launch = false;
+      } else {
+        this._snackBar.open(
+          'The minimum date is greater than the maximum date',
+          this.actionSnackBar,
+          this.optionsSnackBar
+        );
+      }
+    } else {
+      this._snackBar.open(
+        'The minimum mass is greater than the maximum mass',
+        this.actionSnackBar,
+        this.optionsSnackBar
+      );
     }
-    this.sendData.emit(params);
+  }
+
+  stopAnimation() {
+    this.launch = true;
+    this.cancelAnimation = false;
+    this.cancelEvent.emit(true);
   }
 }
