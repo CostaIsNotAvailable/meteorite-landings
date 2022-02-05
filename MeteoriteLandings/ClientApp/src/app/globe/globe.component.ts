db4758462680e92e82461e0c255bfeb3e6fffe33
Fyleek <odecloquement@gmail.com>
2022-02-05 19:22:53 +0100

    Add cancelAnimation Input, change method to display data when animation is cancel

--- MeteoriteLandings/ClientApp/src/app/globe/globe.component.ts
+++ MeteoriteLandings/ClientApp/src/app/globe/globe.component.ts
@@ -2,7 +2,6 @@ import { Component, Input, OnInit } from '@angular/core';
 import ThreeGlobe from 'three-globe';
 import * as THREE from 'three';
 import { TrackballControls } from 'three/examples/jsm/controls/TrackballControls.js';
-import { Material } from 'three';
 import { MeteoriteLanding } from 'src/app/model/data.model';
 
 interface ExtendedGlobeMaterial {
@@ -22,12 +21,11 @@ interface LabelData {
 @Component({
   selector: 'app-globe',
   templateUrl: './globe.component.html',
-  styleUrls: ['./globe.component.scss']
+  styleUrls: ['./globe.component.scss'],
 })
 export class GlobeComponent implements OnInit {
-
   @Input()
-  set displayData(displayData: MeteoriteLanding[]){
+  set displayData(displayData: MeteoriteLanding[]) {
     this._displayData = displayData;
     this.setMeteoritesOnGlobe();
   }
@@ -36,71 +34,102 @@ export class GlobeComponent implements OnInit {
     return this._displayData;
   }
 
+  @Input()
+  set isCanceled(isCanceled: boolean) {
+    this._isCanceled = isCanceled;
+  }
+
+  get isCanceled(): boolean {
+    return this._isCanceled;
+  }
+
   private _displayData!: MeteoriteLanding[];
+  private _isCanceled!: boolean;
   Globe = new ThreeGlobe();
 
-  async setMeteoritesOnGlobe(): Promise<void>{
-    // Gen random data
+  async setMeteoritesOnGlobe(): Promise<void> {
     let labelsData: LabelData[] = [];
+    let labelsDataWhenCancel: LabelData[] = [];
 
     let i = 1;
     const delay = 1000;
     const maxPropagation = 3;
     for (const item of this._displayData) {
-
       i++;
-      const randomizerOVER9000 = Math.random()*5;
-      await setTimeout(() => {
-        labelsData.push(
-          {
+      const randomizerOVER9000 = Math.random() * 5;
+      labelsDataWhenCancel.push({
+        lat: item.recLat,
+        lng: item.recLong,
+        labelColor: 'white',
+        size: 0.15,
+        text: item.name + ' - ' + item.year.substring(0, 4),
+      });
+      if (!this._isCanceled) {
+        await setTimeout(() => {
+          labelsData.push({
             lat: item.recLat,
             lng: item.recLong,
             labelColor: 'white',
             size: 0.15,
-            text: item.name + ' - ' + item.year.substring(0,4) // TODO => onHover labelDot => See the informations
+            text: item.name + ' - ' + item.year.substring(0, 4),
+          });
+          if (!this._isCanceled) {
+            const maxR = Math.min(Math.random() * item.mass, maxPropagation);
+            const ringData = [
+              {
+                lat: item.recLat,
+                lng: item.recLong,
+                maxR: maxR,
+                propagationSpeed: maxR / (maxPropagation / 2),
+                repeatPeriod: 2500,
+              },
+            ];
+            const arcsData = [
+              {
+                startLat: item.recLat + randomizerOVER9000,
+                startLng: item.recLong + randomizerOVER9000 * 2,
+                endLat: item.recLat,
+                endLng: item.recLong,
+                altitude: 1,
+                color: 'yellow',
+              },
+            ];
+            this.Globe.arcsData(arcsData)
+              .arcColor('color')
+              .arcDashLength(0.4)
+              .arcDashGap(randomizerOVER9000)
+              .arcDashInitialGap(randomizerOVER9000 * 0.16)
+              .arcDashAnimateTime(delay)
+              .ringsData(ringData)
+              .ringColor(() => '#F82306')
+              .ringMaxRadius('maxR')
+              .ringPropagationSpeed('propagationSpeed')
+              .ringRepeatPeriod('repeatPeriod');
+            this.Globe.labelsData(labelsData)
+              .labelText('text')
+              .labelSize('size')
+              .labelDotRadius(0.5 / 5)
+              .labelColor('labelColor');
+          } else {
+            this.Globe.arcsData([]);
+            this.Globe.ringsData([]);
+            this.Globe.labelsData([]);
+            this.Globe.labelsData(labelsDataWhenCancel.slice(800))
+              .labelText('text')
+              .labelSize('size')
+              .labelDotRadius(0.5 / 5)
+              .labelColor('labelColor');
           }
-        );
-        const maxR = Math.min(Math.random()*item.mass,maxPropagation);
-        const ringData = [{
-            lat: item.recLat,
-            lng: item.recLong,
-            maxR: maxR,
-            propagationSpeed: maxR/(maxPropagation/2),
-            repeatPeriod: 2500
-        }]
-        const arcsData = [{
-          startLat: item.recLat + randomizerOVER9000,
-          startLng: item.recLong + randomizerOVER9000*2,
-          endLat: item.recLat,
-          endLng: item.recLong,
-          altitude: 1,
-          color: 'yellow'
-        }];
-  
-        this.Globe.arcsData(arcsData)
-        .arcColor('color')
-        .arcDashLength(0.4)
-        .arcDashGap(randomizerOVER9000)
-        .arcDashInitialGap(randomizerOVER9000*0.16)
-        .arcDashAnimateTime(delay)
-        .ringsData(ringData)
-        .ringColor(() => '#F82306')
-        .ringMaxRadius('maxR')
-        .ringPropagationSpeed('propagationSpeed')
-        .ringRepeatPeriod('repeatPeriod');
-        this.Globe.labelsData(labelsData)
-          .labelText('text')
-          .labelSize('size')
-          .labelDotRadius(0.5/5)
-          .labelColor('labelColor')
+        }, i * delay);
       }
-      ,i * delay);
-    };
+    }
   }
 
   ngOnInit(): void {
     this.Globe = new ThreeGlobe()
-      .globeImageUrl('//unpkg.com/three-globe/example/img/earth-blue-marble.jpg')
+      .globeImageUrl(
+        '//unpkg.com/three-globe/example/img/earth-blue-marble.jpg'
+      )
       .bumpImageUrl('//unpkg.com/three-globe/example/img/earth-topology.png');
 
     const directionalLight = new THREE.DirectionalLight(0xffffff, 0.6);
@@ -108,7 +137,7 @@ export class GlobeComponent implements OnInit {
 
     // Render
     const render = new THREE.WebGLRenderer();
-    render.setSize(window.innerWidth/1.101, window.innerHeight);
+    render.setSize(window.innerWidth / 1.201, window.innerHeight);
     const globeViz = document.getElementById('globeViz');
     if (globeViz) {
       globeViz.appendChild(render.domElement);
@@ -122,15 +151,15 @@ export class GlobeComponent implements OnInit {
 
     // Camera
     const camera = new THREE.PerspectiveCamera();
-    camera.aspect = window.innerWidth/window.innerHeight;
+    camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
     camera.position.z = 280; // Zoom camera
 
     // Camera controls
     const tbControls = new TrackballControls(camera, render.domElement);
     tbControls.minDistance = 101;
-    tbControls.rotateSpeed = 10;
-    tbControls.zoomSpeed = 1;
+    tbControls.rotateSpeed = 5;
+    tbControls.zoomSpeed = 0.8;
 
     (function animate() {
       tbControls.update();
@@ -138,5 +167,4 @@ export class GlobeComponent implements OnInit {
       requestAnimationFrame(animate);
     })();
   }
-
 }
